7b0e99f0ae450b1c48bdc1f1d9b62def544a6efd
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2024-09-25 10:00:02 +0200

    Tutorial GraphQL
    Updated code with basic tutorial for GraphQL

---
+++ movie/resolvers.py
@@ -0,0 +1,22 @@
+import json
+
+def movie_with_id(_,info,_id):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['id'] == id:
+                return movie
+            
+def update_movie_rate(_,info,_id,_rate):
+    newmovies = {}
+    newmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movie['rating'] = _rate
+                newmovie = movie
+                newmovies = movies
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile)
+    return newmovie
